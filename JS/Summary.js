b6394df942c85632b726d658967938cc9740da0c
Maryann Coynoypopoylos <mary.greek@hotmail.com>
2023-11-22 20:54:00 -0330

    Trying out different methods to display vlaues from local storage in summary containers.

--- JS/Summary.js
+++ JS/Summary.js
@@ -1,14 +1,36 @@
 // Pulled Data from local storage
+// Write this for each key 
+const autoandtransport = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+const home = JSON.parse(localStorage.getItem('homeanswers',));
 const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
-console.log(retrievedData);
+const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+const retrievedData = JSON.parse(localStorage.getItem('autoandtransportanswers',));
+
+
+console.log(retrievedData)
+console.log(autoandtransport)
 
 // // Pulled values only from object in local storage
 const values = Object.values(retrievedData);
-console.log(values);
+// console.log(values);
+
+
 
 // // Create Function
 function displayValues(retrievedData) {
-    const summaryContainer = document.getElementById('summary-container');
+    const summaryContainer = document.getElementById('table.autandtransport');
+    const summaryContainer = document.getElementById('table.home');
+    const summaryContainer = document.getElementById('table');
+    const summaryContainer = document.getElementById('table');
+    const summaryContainer = document.getElementById('table');
+    const summaryContainer = document.getElementById('table');
+    const summaryContainer = document.getElementById('table');
+    const summaryContainer = document.getElementById('table');
+    const summaryContainer = document.getElementById('table');
 
     // Create Heading
     let content = '<p>Auto and Transport</p>';
